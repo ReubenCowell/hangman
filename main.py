93f7d1fce607b9a344b4699e03816e327df3c28a
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2023-08-28 20:50:03 +0100

    added the ability to make a guess and what happens when guess is wrong

--- main.py
+++ main.py
@@ -13,20 +13,41 @@ def get_word():
 		f.close()
 	return in_word, len(in_word), list(in_word)
 
+def get_guess():
+    initial_guess = input("Please enter your guess >>> ")
+    while len(initial_guess) > 1 or initial_guess.isalpha() == False:
+            initial_guess = input("Guess must be a singular letter >>> ")
+    return initial_guess
+
+def status_update():
+    print("----------\nLives: " + str(lives) + "\nBad guesses: " + ", ".join(bad_guesses) + "\nCurrent word: " + " ".join(unguessed_list))
+
 
 print("Welcome to Hangman, created by Reuben Cowell")
 ready = input("Are you ready to play? (y/n) >>> ")
 
 while ready == "y":
-	lives = 6
-	bad_guesses = []
-	unguessed_list = ["_"]
-	word, len_word, char_list = get_word()
-	#print(word, len_word, char_list)
-	print("You have " + str(lives) + " lives!")
-	unguessed_list = unguessed_list * len_word
-	print(" ".join(unguessed_list))
-		
+    lives = 6
+    bad_guesses = []
+    word, len_word, char_list = get_word()
+    unguessed_list = ["_"] * len_word
+    print(word, len_word, char_list)
 
-	ready = input("Do you still want to play? (y/n) >>> ")
+    print("\n---------------------- \nYou have " + str(lives) + " lives!")
+    print("Current word: " + " ".join(unguessed_list))
+
+    while lives > 0:
+        guess = get_guess()
+        if char_list.count(guess) == 0:
+            print("Unlucky, guess not in word.\nLife lost")
+            bad_guesses.append(guess)
+            lives -= 1
+            status_update()
+        #elif char_list.count(guess) == 1:
+        #    print("Correct! Letter in word!")
+
+
+
+    ready = input("Good game \nDo you want to play again (y/n) >>> ")
+
 print("bye then")
