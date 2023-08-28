875afd291a239e2ae62807648fc8eb6fbf9f7275
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2023-08-28 18:25:57 +0100

    Initial attempts to try and check the words

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 # Reuben Cowell
 # This is my GCSE NEA project submission: Hangman
 
-print("this is a test")
+import json # json is used to store my word list
+import random # random is used to pick a random word from the json file
+
+
+with open('wordlist1.json', 'r') as f:
+    word = " "
+    while len(word) < 3 or len(word) > 10:
+        print(word)
+        word = random.choice(json.load(f))
+f.close()
+
+print(word, len(word))
