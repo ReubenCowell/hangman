9020aa98df9ea4bc29d300f001140913e9a08c28
Reuben Cowell <reuben.reubencowell.cowell@gmail.com>
2023-08-28 18:46:37 +0100

    finished and refactored get_word function

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ import json # json is used to store my word list
 import random # random is used to pick a random word from the json file
 
 
-with open('wordlist1.json', 'r') as f:
-    word = " "
-    while len(word) < 3 or len(word) > 10:
-        print(word)
-        word = random.choice(json.load(f))
-f.close()
+def get_word():
+    in_word = ""
+    while len(in_word) < 3 or len(in_word) > 10:
+        with open('wordlist.json', 'r') as f:
+            in_word = random.choice(json.load(f))
+        f.close()
+    return in_word, len(in_word)
 
-print(word, len(word))
+
+word, len_word = get_word()
+
+print(word, len_word)
