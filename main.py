ddc00e2849c198fdbb5f996772e7f6f56884d7b8
ReubenCowell <88235356+ReubenCowell@users.noreply.github.com>
2023-09-06 11:20:02 +0000

    work on a correct guess

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import random  # random is used to pick a random word from the json file
 
 
 def get_word():
-        # this function will get a random word from the word list and returns the word, the length of the word and the word as a list of characters 
+	# this function will get a random word from the word list and returns the word, the length of the word and the word as a list of characters
 	in_word = ""
 	while len(in_word) < 3 or len(in_word) > 10:
 		with open('wordlist.json', 'r') as f:
@@ -14,41 +14,52 @@ def get_word():
 		f.close()
 	return in_word, len(in_word), list(in_word)
 
+
 def get_guess():
-    initial_guess = input("Please enter your guess >>> ")
-    while len(initial_guess) > 1 or initial_guess.isalpha() == False:
-            initial_guess = input("Guess must be a singular letter >>> ")
-    return initial_guess
+	initial_guess = input("Please enter your guess >>> ")
+	while len(initial_guess) > 1 or initial_guess.isalpha() == False:
+		initial_guess = input("Guess must be a singular letter >>> ")
+	return initial_guess
+
 
 def status_update():
-    print("----------\nLives: " + str(lives) + "\nBad guesses: " + ", ".join(bad_guesses) + "\nCurrent word: " + " ".join(unguessed_list))
+	print("----------\nLives: " + str(lives) + "\nBad guesses: " +
+	      ", ".join(bad_guesses) + "\nCurrent word: " + " ".join(unguessed_list))
 
 
 print("Welcome to Hangman, created by Reuben Cowell")
 ready = input("Are you ready to play? (y/n) >>> ")
 
 while ready == "y":
-    lives = 6  # reset lives at the start of the game
-    bad_guesses = [] # resets a list of bad guessed lessons 
-    word, len_word, char_list = get_word() # this runs the guess word function
-    unguessed_list = ["_"] * len_word # this sets up a list of underscores
-    print(word, len_word, char_list)
+	lives = 6  # reset lives at the start of the game
+	bad_guesses = []  # resets a list of bad guessed lessons
+	word, len_word, char_list = get_word()  # this runs the guess word function
+	unguessed_list = ["_"] * len_word  # this sets up a list of underscores
+	print(word, len_word, char_list)
 
-    print("\n---------------------- \nYou have " + str(lives) + " lives!")
-    print("Current word: " + " ".join(unguessed_list))
+	print("\n---------------------- \nYou have " + str(lives) + " lives!")
+	print("Current word: " + " ".join(unguessed_list))
 
-    while lives > 0:
-        guess = get_guess()
-        if char_list.count(guess) == 0:
-            print("Unlucky, guess not in word.\nLife lost")
-            bad_guesses.append(guess)
-            lives -= 1
-            status_update()
-        #elif char_list.count(guess) == 1:
-        #    print("Correct! Letter in word!")
+	while lives > 0:
+		guess = get_guess()
+		if guess in bad_guesses:
+			print('Already guessed: \n' + ", ".join(bad_guesses) + '\n')
+		elif char_list.count(guess) == 0:
+			print("Unlucky, guess not in word.\nLife lost\n")
+			bad_guesses.append(
+			 guess)  # adds the incrrect guess to the list of bad guesses
+			lives -= 1
+			status_update()
+		elif guess in char_list:
+			if char_list.count(guess) > 1:
+				print('more than one letter')
+			else:
+				guess_loc = char_list.index(guess)
+				print(guess_loc)
 
+#elif char_list.count(guess) == 1:
+#    print("Correct! Letter in word!")
 
-
-    ready = input("Good game \nDo you want to play again (y/n) >>> ")
+	ready = input("Good game \nDo you want to play again (y/n) >>> ")
 
 print("bye then")
