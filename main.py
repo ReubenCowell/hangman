293fd0a9882483c823c418b55667ae457188bc1b
ReubenCowell <88235356+ReubenCowell@users.noreply.github.com>
2023-09-18 08:59:56 +0000

    Added comments and a way to show you've won

--- main.py
+++ main.py
@@ -15,14 +15,14 @@ def get_word():
 	return in_word, len(in_word), list(in_word)
 
 
-def get_guess():
+def get_guess(): # This is used to get a single letter guess from a user, and will keep trying to get a guess until it is 'legal'
 	initial_guess = input("Please enter your guess >>> ")
 	while len(initial_guess) > 1 or initial_guess.isalpha() == False:
 		initial_guess = input("Guess must be a singular letter >>> ")
 	return initial_guess
 
 
-def status_update():
+def status_update(): # this prints to the screen a user's current lives and their bad guesses
 	print("----------\nLives: " + str(lives) + "\nBad guesses: " +
 	      ", ".join(bad_guesses) + "\nCurrent word: " + " ".join(unguessed_list))
 
@@ -35,31 +35,42 @@ while ready == "y":
 	bad_guesses = []  # resets a list of bad guessed lessons
 	word, len_word, char_list = get_word()  # this runs the guess word function
 	unguessed_list = ["_"] * len_word  # this sets up a list of underscores
+	progress = 0 # a counter that shows how many letters in a word have been counted - wil be compared against word len
+	won = False
 	print(word, len_word, char_list)
 
 	print("\n---------------------- \nYou have " + str(lives) + " lives!")
 	print("Current word: " + " ".join(unguessed_list))
 
-	while lives > 0:
+	while lives > 0 and won == False:
 		guess = get_guess()
-		if guess in bad_guesses:
+		if guess in bad_guesses: # if the guess has been already guessed by user, it asks the user to guess again
 			print('Already guessed: \n' + ", ".join(bad_guesses) + '\n')
-		elif char_list.count(guess) == 0:
+		elif char_list.count(guess) == 0: # if the guess is incorrect, it will take a life and add the bad guess to a bad word list
 			print("Unlucky, guess not in word.\nLife lost\n")
-			bad_guesses.append(
-			 guess)  # adds the incrrect guess to the list of bad guesses
+			bad_guesses.append(guess)  # adds the incrrect guess to the list of bad guesses
 			lives -= 1
-			status_update()
+			status_update() # prints the progress to the screen
 		elif guess in char_list:
-			if char_list.count(guess) > 1:
-				print('more than one letter')
+			if char_list.count(guess) > 1: # TODO 
+				print('The letter you guess')
 			else:
 				guess_loc = char_list.index(guess)
-				print(guess_loc)
-
-#elif char_list.count(guess) == 1:
-#    print("Correct! Letter in word!")
+				unguessed_list[guess_loc] = guess 
+				print("\nCorrect! Letter in word!\n")
+				status_update()
+				progress += 1
+				if progress == len_word:
+					print('you won')
+					won = True
+					#TODO: Add a way to exit the while loop when user wins
+		if lives == 0:
+			print("GAME OVER! \nThe word was: "+word)
 
 	ready = input("Good game \nDo you want to play again (y/n) >>> ")
 
 print("bye then")
+
+
+# TODO: tidy up what is printed to the user
+# TODO: test program 
